7273186a7e9884d33d88807a8a1f53c25fbef316
CrioUser <crio-user@criodo.com>
2023-04-28 18:10:31 +0530

    complete project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import LandingPage from './components/LandingPage';
+import VideoPage from './components/VideoPage';
+import {
+  Switch,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+
+export const config = {API_URL: `https://5decb229-1601-49ce-b144-353bcf14db63.mock.pstmn.io`};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+   <Switch>
+    <Route exact path="/">
+      <LandingPage />
+    </Route>
+    <Route path="/video/:id">
+      <LandingPage onVideoPage />
+    </Route>
+  </Switch>
+  </BrowserRouter>
   );
 }
 
 export default App;
+
